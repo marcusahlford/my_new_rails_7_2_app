654cad73c9a9575387e4f764c917c87dd3c4c8a6
Marcus Ahlford <marcus.ahlford@gmail.com>
2024-05-13 21:47:03 +0200

    Temp fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def s
-    b = 3
-
-    2 + "a"
-  end
+  # Temp fix, see:
+  # https://discuss.rubyonrails.org/t/project-doesnt-run-in-github-codespaces/84046/5
+  skip_before_action :verify_authenticity_token, if: -> { Rails.env.development? }
 end
