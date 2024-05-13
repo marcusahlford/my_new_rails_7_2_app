8bc0e3cd19dea3ae6d3e78a7305edd66302f5456
Marcus Ahlford <marcus.ahlford@gmail.com>
2024-05-13 16:30:51 +0200

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  def s
+    b = 3
+
+    2 + "a"
+  end
+end
